c21eeaebec30c9813c9cccee1ce8bd1d7714ca8f
Jean woinaroskI <Jwoinaroskii@gmail.com>
2024-12-19 15:42:57 -0300

    meu primeiro commit

---
+++ js/Untitled-1.js
@@ -0,0 +1,5 @@
+let cpfBloqueado = "155.444.235.45"
+let cpfUsuario = "222.111.222-09"
+let ehCPFBloqueado = cpfUsuario === cpfBloqueado
+console.log("O usuario pode embarcar? " + ehCPFBloqueado)
+
---
+++ js/let idade = 30.js
@@ -0,0 +1,55 @@
+let idade = 30
+
+console.log("A minha idade é " + idade)
+
+let codigoDoProduto = 10023
+let codigoExato= codigoDoProduto - 1000
+
+console.log( "O codigo do produto" + codigoExato)
+console.log("O meu codigo de comando é " + codigoDoProduto)
+idade = 30 + 6
+
+console.log("A minha idade é " + idade)
+
+
+
+
+let primeiroNumero = 1023
+let segundoNumero = 2023
+console.log(primeiroNumero - segundoNumero + segundoNumero + primeiroNumero +100000)
+
+
+
+let precoProduto = 100.99
+let valorComTaxa = precoProduto * 2
+
+console.log("O preço com taixa é" + valorComTaxa)
+
+
+
+
+
+let multiplicador = 4
+let multiplicando = 12
+let produto = multiplicador * multiplicando + multiplicador
+
+console.log("Resultado da multiplicaçao é " + produto)
+
+
+multiplicador = 8
+produto = multiplicador * multiplicando
+
+console.log("resultado da multiplicação é " + produto)
+
+
+let notaDoMercado = 50
+console.log("O teu amigo troxa vai pagar " + notaDoMercado / 2)
+
+let pessoaParaDividir = 2
+
+console.log( notaDoMercado / pessoaParaDividir)
+
+let calculo = 10 % 3
+console.log("Operação de módulo " + calculo)
+
+///////////
